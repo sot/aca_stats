c8ca2dc8ebadd8fb12c8860559010cbb5da3551a
bveg <bvegetabile@gmail.com>
2014-08-19 09:28:38 -0400

    Updated R_compare with a case for the Pima indians dataset for a probit model

--- R_compare.R
+++ R_compare.R
@@ -3,9 +3,13 @@ fit <- glm(chd ~ sbp+tobacco+ldl+factor(famhist)+obesity+alcohol+age, family='bi
 summary(fit)
 
 f47<-read.csv('data/finney1947.csv')
-fit2 <- glm(Y ~ Volume+Rate, family='binomial', data=f47, trace=TRUE)
+fit2 <- glm(Y ~ Volume+Rate, family=binomial(link = "logit"), data=f47)
 summary(fit2)
 
 pima<-read.csv('data/PimaIndians.csv')
 fit3 <- glm(type ~ npreg+glu+bp+skin+bmi+ped, family='binomial', data=pima, trace=TRUE)
 summary(fit3)
+
+pima<-read.csv('data/PimaIndians.csv')
+fit4 <- glm(type ~ npreg+glu+bp+skin+bmi+ped, family=binomial(link = "probit"), data=pima, trace=TRUE)
+summary(fit4)
